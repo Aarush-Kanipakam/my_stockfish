5b95f9b0657b3209bb2c0712a8d66ebb9c3d153e
Aarush-Kanipakam <157634160+Aarush-Kanipakam@users.noreply.github.com>
2025-01-29 02:12:11 +0530

    Update types.h

--- src/types.h
+++ src/types.h
@@ -101,8 +101,8 @@ constexpr bool Is64Bit = false;
 typedef uint64_t Key;
 typedef uint64_t Bitboard;
 
-constexpr int MAX_MOVES = 256;
-constexpr int MAX_PLY   = 246;
+constexpr int MAX_MOVES = 128;
+constexpr int MAX_PLY   = 123;
 
 /// A move needs 16 bits to be stored
 ///
